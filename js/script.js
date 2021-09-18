bd4a6b29f789ea608998b2cf014bf723655e2027
fazi1live <faziislive@gmail.com>
2021-09-18 17:29:27 +0500

    files added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function kitchensWardrobs(){
+    window.location.href = 'kitchens&wardrobs.html';
+}
+function mediaWall(){
+    window.location.href = 'media-wall&console.html';
+}
+function appliances(){
+    window.location.href = '';
+}
